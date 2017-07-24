7d7c8c2fe1ef12d19ee7d46ac71a24405269456d
Nick Cresner <nickcresner@gmail.com>
2017-07-24 11:00:12 +0100

    added base64 image upload

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,5 @@ class Trip < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :legs, dependent: :destroy
   has_and_belongs_to_many :attendees, class_name: "User", join_table: "trips_users"
+  mount_uploader :image, ImageUploader
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   has_many :trips_created
   has_many :trips_attending
-  attributes :id, :username, :firstname, :lastname, :email, :bio, :image, :facebook_id
+  attributes :id, :username, :firstname, :lastname, :email, :bio, :image, :facebook_id, :image_src
+
+  def image_src
+    object.image.url
+  end
 end
