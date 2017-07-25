8ef087328fdd73ee7c23b69d16173b17037e10be
Nick Cresner <nickcresner@gmail.com>
2017-07-25 14:17:54 +0100

    budget planner api backend

---
+++ app/controllers/budget_your_trip_controller.rb
@@ -0,0 +1,25 @@
+class BudgetYourTripController < ApplicationController
+
+  skip_before_action :authenticate_user!
+
+  def budget
+    country_data = HTTParty.get("http://www.budgetyourtrip.com/api/v3/search/geo/#{params[:lat]}/#{params[:lng]}",
+      {
+      headers: { "X-API-KEY": ENV['BUDGET_YOUR_TRIP_KEY'], "Accept" => "application/json"}
+    }).parsed_response
+
+    country_code = country_data["data"][0]["country_code"]
+
+    p country_code
+
+    daily_budget = HTTParty.get("http://www.budgetyourtrip.com/api/v3/costs/country/#{country_code}",
+      {
+        headers: { "X-API-KEY": ENV['BUDGET_YOUR_TRIP_KEY'], "Accept" => "application/json"}
+      }).parsed_response
+
+    p daily_budget
+    render json: daily_budget["data"].last, status: :ok
+  end
+
+
+end
