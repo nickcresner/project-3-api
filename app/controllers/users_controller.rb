39900d9810f0c7d25f7658c72c4dc065d70befcc
Nick Cresner <nickcresner@gmail.com>
2017-07-25 11:43:30 +0100

    user authentication bugfix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :firstname, :lastname, :email, :base64)
+      params.require(:user).permit(:username, :firstname, :lastname, :email, :base64, :bio)
     end
 end
