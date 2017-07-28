e60811e2f04d4a62b2961fe9f18d9007a05baf55
Nick Cresner <nickcresner@gmail.com>
2017-07-28 10:08:39 +0100

    fixing deployment bug

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:7000', 'https://adventurefy.herokuapp.com/']
+    origins '*'
 
     resource '*',
       headers: :any,
